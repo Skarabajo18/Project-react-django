fe2a45ce4e638a50c9aab1d4c781b4171787ff2a
Skarabajo18 <felipesegovia14@gmail.com>
2023-04-02 23:47:35 -0400

    Initial commit

---
+++ Front/src/App.js
@@ -0,0 +1,13 @@
+/* import logo from './logo.svg'; */
+import './App.css';
+import Dashboard from './Dashboard';
+import Post from './Post';
+function App() {
+  return (
+    <Dashboard>
+
+    </Dashboard>
+  );
+}
+
+export default App;
---
+++ Front/src/Post.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Post() {
+  return (
+
+    <div class="card shadow mb-4">
+       
+        <div
+            class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 class="m-0 font-weight-bold text-primary">Post</h6>
+            <div class="dropdown no-arrow">
+                <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                    aria-labelledby="dropdownMenuLink">
+                    <div class="dropdown-header">Dropdown Header:</div>
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+        </div>
+       
+        <div class="card-body">
+            Dropdown menus can be placed in the card header in order to extend the functionality
+            of a basic card. In this dropdown card example, the Font Awesome vertical ellipsis
+            icon in the card header can be clicked on in order to toggle a dropdown menu.
+        </div>
+    </div>
+  )
+}
+
+export default Post
